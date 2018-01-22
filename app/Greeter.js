cdd714d94930a2f6f1c5b3542e724b49c017e82f
vincent-xw <jiaxuewen2006@163.com>
2018-01-22 08:25:41 +0800

    init

--- app/Greeter.js
+++
@@ -1,9 +0,0 @@
-// Greeter.js
-var config = require('./config.json');
-import styles from './Greeter.css';
-
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
--- app/main.js
+++
@@ -1,5 +0,0 @@
-//main.js
-import './main.css';
- 
-const greeter = require('./Greeter.js');
-document.querySelector("#root").appendChild(greeter());
