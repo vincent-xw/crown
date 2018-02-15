72cadf55c0f004f468bc3b97f521330643efff68
vincent-xw <jiaxuewen2006@163.com>
2018-02-16 03:00:16 +0800

    debug

--- frontend/method/ws.js
+++ frontend/method/ws.js
@@ -1,6 +1,9 @@
 module.exports = (wss,data)=>{
   const WebSocket = require('ws');
   wss.clients.forEach(function each(client) {
+    console.log("client.readyState"+client.readyState);
+    console.log("WebSocket.OPEN"+WebSocket.OPEN);
+    
     if (client.readyState === WebSocket.OPEN) {
       client.send(data);
       
