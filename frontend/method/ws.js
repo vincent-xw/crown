e11193b61ef1fc6338e1ab8da3d3d5d3a166a5d1
vincent-xw <jiaxuewen2006@163.com>
2018-02-16 19:52:08 +0800

    注释log

--- frontend/method/ws.js
+++ frontend/method/ws.js
@@ -1,6 +1,6 @@
 module.exports = (wss,data)=>{
   const WebSocket = require('ws');
-  console.log(wss.clients.length);
+  // console.log(wss.clients.length);
   wss.clients.forEach(function each(client) {
     
     if (client.readyState === WebSocket.OPEN) {
